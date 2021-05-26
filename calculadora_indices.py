d88fe73675692f663a927d6fd12943d969b55e13
David Felipe Ríos Robby <50936072+feliperi1@users.noreply.github.com>
2021-05-26 16:11:00 -0500

    Create calculadora_indices.py

---
+++ calculadora_indices.py
@@ -0,0 +1,7 @@
+#Funcion para calcular el IMC
+def calcular_IMC(peso:float,altura:float)->float:
+    return
+
+def calcular_porcentaje_grasa(peso:float,altura:float,edad:int,valor_genero:float)->float:
+    return
+
